ba5c036496b38f983b72e095fcca0b82005e4333
mc2engel <mcengel@ualberta.ca>
2022-08-25 16:57:29 -0400

    minor bug in forward.py, beginning to add langevin

--- src/forward.py
+++ src/forward.py
@@ -1,5 +1,6 @@
 import pdb
 
+
 from jax import jit
 from jax import random
 from jax.config import config as jax_config
@@ -9,6 +10,7 @@ from jax_md import simulate
 from jax_md import space
 from jax_md import util
 from jax_md.rigid_body import RigidBody, Quaternion
+from jax_md import quantity
 
 from utils import DEFAULT_TEMP
 from utils import com_to_backbone, com_to_stacking, com_to_hb
@@ -23,6 +25,7 @@ from energy import energy_fn_factory
 from jax.config import config
 config.update("jax_enable_x64", True)
 
+import langevin
 
 f64 = util.f64
 
@@ -39,13 +42,13 @@ back_site = jnp.array(
 
 
 
-def forward(top_info, config_info, steps, t=DEFAULT_TEMP):
+def forward(top_info, config_info, steps, t=DEFAULT_TEMP, mgamma=0.1, sim_type="langevin"):
     body = config_info.states[0]
 
     seq = jnp.array(get_one_hot(top_info.seq), dtype=f64)
     n = top_info.n
 
-    displacement, shift = space.periodic(config_info.box_size)
+    displacement, shift_fn = space.periodic(config_info.box_size)
     key = random.PRNGKey(0)
     # key, pos_key, quat_key = random.split(key, 3)
 
@@ -56,16 +59,18 @@ def forward(top_info, config_info, steps, t=DEFAULT_TEMP):
     params = get_default_params(t=t)
 
     # Simulate with the energy function via Nose-Hoover
-    kT = get_kt(t=temp) # 300 Kelvin = 0.1 kT
+    kT = get_kt(t=t) # 300 Kelvin = 0.1 kT
     dt = 5e-3
 
-    init_fn, step_fn = simulate.nvt_nose_hoover(energy_fn, shift, dt, kT)
+    if(sim_type == "langevin"):
+       init_fn, step_fn = langevin.nvt_langevin(energy_fn, shift_fn, dt, kT, mgamma) 
+       state = init_fn(key, body, mass=mass, seq=seq, params=params)
+    elif(sim_type=="nose_hoover"):
+       init_fn, step_fn = simulate.nvt_nose_hoover(energy_fn, shift_fn, dt, kT)
+       state = init_fn(key, body, mass=mass, seq=seq, params=params)
+       E_initial = simulate.nvt_nose_hoover_invariant(energy_fn, state, kT, seq=seq, params=params)
 
     step_fn = jit(step_fn)
-
-    state = init_fn(key, body, mass=mass, seq=seq, params=params)
-    E_initial = simulate.nvt_nose_hoover_invariant(energy_fn, state, kT,
-                                                   seq=seq, params=params)
 
     trajectory = [state.position]
 
@@ -73,14 +78,14 @@ def forward(top_info, config_info, steps, t=DEFAULT_TEMP):
         state = step_fn(state, seq=seq, params=params)
         trajectory.append(state.position)
 
-    pdb.set_trace()
+    #pdb.set_trace()
 
     # E_final = simulate.nvt_nose_hoover_invariant(energy_fn, state, kT)
 
     # FIXME: store in TrajectoryInfo and write to file (potentially also write topology file)
 
     print("done")
-
+    return trajectory
 
 if __name__ == "__main__":
 
@@ -88,14 +93,18 @@ if __name__ == "__main__":
     conf_path = "data/polyA_10bp/equilibrated.dat"
     top_path = "data/polyA_10bp/generated.top"
     """
-
+    """
     conf_path = "/home/ryan/Documents/Harvard/research/brenner/jaxmd-oxdna/data/simple-helix/start.conf"
     top_path = "/home/ryan/Documents/Harvard/research/brenner/jaxmd-oxdna/data/simple-helix/generated.top"
-
+    """
+    
+    conf_path = "/Users/megancengel/Research_apps/jaxmd-oxdna/data/simple-helix/start.conf"
+    top_path = "/Users/megancengel/Research_apps/jaxmd-oxdna/data/simple-helix/generated.top"
+    
     top_info = TopologyInfo(top_path, reverse_direction=True)
     config_info = TrajectoryInfo(top_info, traj_path=conf_path, reverse_direction=True)
 
-    forward(top_info, config_info, steps=10)
+    forward(top_info, config_info, steps=10, sim_type="langevin")
 
     """
     TODO:
