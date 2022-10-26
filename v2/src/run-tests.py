baea1c290b14f08fcee562658ec4f55b1e2034a6
Ryan Krueger <ryankrue@umich.edu>
2022-10-26 11:21:00 -0400

    Added simple-helix test

--- v2/src/run-tests.py
+++ v2/src/run-tests.py
@@ -10,5 +10,11 @@ def run():
     input_path = basedir / "input"
     test_subterms.run(top_path, traj_path, input_path)
 
+    basedir = Path("v2/data/test-data/simple-helix")
+    traj_path = basedir / "output.dat"
+    top_path = basedir / "generated.top"
+    input_path = basedir / "input"
+    test_subterms.run(top_path, traj_path, input_path)
+
 if __name__ == "__main__":
     run()
