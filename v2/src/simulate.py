51dd200951548a9e4ecf41098d775554ecc748b8
Ryan Krueger <ryankrue@umich.edu>
2022-10-26 11:53:16 -0400

    Option to write to output directory in simulate

--- v2/src/simulate.py
+++ v2/src/simulate.py
@@ -5,12 +5,16 @@ import jax.numpy as jnp
 from jax import random
 from jax.tree_util import Partial
 from tqdm import tqdm
+import datetime
+from pathlib import Path
+import shutil
 
 from jax_md.rigid_body import RigidBody
 from jax_md import space, util
 
 from utils import nucleotide_mass, get_kt, moment_of_inertia, get_one_hot, DEFAULT_TEMP
 from utils import base_site, stack_site, back_site
+from utils import bcolors
 import langevin
 from energy import factory
 from loader import get_params
@@ -20,7 +24,32 @@ from loader.topology import TopologyInfo
 
 f64 = util.f64
 
-def run_single_langevin(top_path, conf_path, n_steps, key, T=DEFAULT_TEMP, dt=5e-3):
+def run_single_langevin(top_path, conf_path,
+                        n_steps, key, T=DEFAULT_TEMP, dt=5e-3, save_every=100,
+                        output_basedir="v2/data/output/", save_output=False):
+
+    if not Path(top_path).exists():
+        raise RuntimeError(f"Topology file does not exist at location: {top_path}")
+
+    if not Path(conf_path).exists():
+        raise RuntimeError(f"Configuration file does not exist at location: {conf_path}")
+
+    if save_output:
+        output_basedir = Path(output_basedir)
+        if not output_basedir.exists():
+            raise RuntimeError(f"Output base directory does not exist at location: {output_basedir}")
+        timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+        run_name = f"langevin_{timestamp}"
+        run_dir = output_basedir / run_name
+        run_dir.mkdir(parents=False, exist_ok=False)
+        shutil.copy(top_path, run_dir)
+        shutil.copy(conf_path, run_dir)
+        params_str = f"topology file: {top_path}\nconfiguration file: {conf_path}\nn_steps: {n_steps}\nkey: {key}\ntemperature: {T}\ndt: {dt}\nsave_every: {save_every}\n"
+        with open(run_dir / "params.txt", "w+") as f:
+            f.write(params_str)
+        print(bcolors.WARNING + f"Created directory and copied simulation information at location: {run_dir}" + bcolors.ENDC)
+
+    print(bcolors.OKBLUE + f"Setting up simulation..." + bcolors.ENDC)
     mass = RigidBody(center=jnp.array([nucleotide_mass]),
                      orientation=jnp.array([moment_of_inertia]))
     gamma = RigidBody(center=jnp.array([DEFAULT_TEMP/2.5]),
@@ -48,27 +77,28 @@ def run_single_langevin(top_path, conf_path, n_steps, key, T=DEFAULT_TEMP, dt=5e
 
     trajectory = [state.position]
     energies = [energy_fn(state.position)]
-    for i in tqdm(range(n_steps)):
+    print(bcolors.OKBLUE + f"Starting simulation..." + bcolors.ENDC)
+    for i in tqdm(range(n_steps), colour="red"): # note: colour can be one of [hex (#00ff00), BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE]
         state = step_fn(state, seq=seq, params=params)
 
-        if i % 1000 == 0:
+        if i % save_every == 0:
             energies.append(energy_fn(state.position))
             trajectory.append(state.position)
 
-    print("Finished Simulation")
     return trajectory, energies
 
 
 
 if __name__ == "__main__":
     import time
+    import numpy as np
 
     top_path = "data/simple-helix/generated.top"
     conf_path = "data/simple-helix/start.conf"
     key = random.PRNGKey(0)
 
     start = time.time()
-    run_single_langevin(top_path, conf_path, n_steps=1000, key=key)
+    run_single_langevin(top_path, conf_path, n_steps=1000, key=key, save_output=True)
     end = time.time()
     total_time = end - start
-    print(f"Execution took: {total_time}")
+    print(bcolors.OKGREEN + f"Finished simulation in {np.round(total_time, 2)} seconds" + bcolors.ENDC)
