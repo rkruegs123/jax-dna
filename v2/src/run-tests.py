b74bd9c05379819ab6a5116df37adc3d989ee98b
Ryan Krueger <ryankrue@umich.edu>
2022-10-31 19:35:08 -0400

    More trest updates

--- v2/src/run-tests.py
+++ v2/src/run-tests.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 
-from test import test_subterms
+from test import test_subterms, test_propeller, test_pitch
 
 
 def run():
+    # Test subterms tests
     basedir = Path("v2/data/test-data/simple-coax")
     traj_path = basedir / "output.dat"
     top_path = basedir / "generated.top"
@@ -15,6 +16,12 @@ def run():
     top_path = basedir / "generated.top"
     input_path = basedir / "input"
     test_subterms.run(top_path, traj_path, input_path)
+
+    # Test propeller twist
+    test_propeller.run()
+
+    # Test pitch
+    test_pitch.run()
 
 if __name__ == "__main__":
     run()
