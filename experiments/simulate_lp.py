dddd01b40750fa7be60ee51d28668ab8976ee619
Ryan Krueger <ryankrue@umich.edu>
2023-08-14 02:01:52 -0400

    Logging during simulation of Lp

--- experiments/simulate_lp.py
+++ experiments/simulate_lp.py
@@ -5,18 +5,22 @@ import time
 import numpy as onp
 from copy import deepcopy
 import functools
+import matplotlib.pyplot as plt
 
 import jax.numpy as jnp
-from jax import jit, vmap, random, lax
+from jax import jit, vmap, random, lax, tree_util
 from jax_md import space, simulate, rigid_body
 
 from jax_dna.common import utils, topology, trajectory
 from jax_dna.dna1 import model
+from jax_dna.loss import persistence_length
 
 from jax.config import config
 config.update("jax_enable_x64", True)
 
 
+def tree_stack(trees):
+    return tree_util.tree_map(lambda *v: jnp.stack(v), *trees)
 
 displacement_fn, shift_fn = space.free()
 dt = 5e-3
@@ -41,7 +45,7 @@ def sim_for_loop(conf_info, top_info, n_steps, key, save_every=1):
                          unbonded_nbrs=top_info.unbonded_nbrs.T)
 
 
-    trajectory = list()
+    traj = list()
     state = deepcopy(init_state)
     for i in tqdm(range(n_steps), colour="green"):
         state = step_fn(state,
@@ -50,10 +54,11 @@ def sim_for_loop(conf_info, top_info, n_steps, key, save_every=1):
                         unbonded_nbrs=top_info.unbonded_nbrs.T)
 
         if i % save_every == 0:
-            trajectory.append(state.position)
-    return trajectory
+            traj.append(state.position)
+    return tree_stack(traj)
 
-def sim_scan(conf_info, top_info, n_steps, key):
+
+def sim_scan(conf_info, top_info, n_steps, key, save_every):
     init_body = conf_info.get_states()[0]
     seq_oh = jnp.array(utils.get_one_hot(top_info.seq), dtype=jnp.float64)
 
@@ -71,7 +76,7 @@ def sim_scan(conf_info, top_info, n_steps, key):
         return state, state.position
 
     fin_state, traj = lax.scan(scan_fn, init_state, jnp.arange(n_steps))
-    return traj
+    return traj[::save_every]
 
 def sim_nested_scan(conf_info, top_info, n_inner_steps, n_outer_steps, key):
 
@@ -118,13 +123,17 @@ def sim_nested_for_scan(conf_info, top_info, n_inner_steps, n_outer_steps, key):
     fori_step_fn = lambda t, state: step_fn(state)
     fori_step_fn = jit(fori_step_fn)
 
-    state = deepcopy(init_state)
-    trajectory = list()
-    for i in tqdm(range(n_outer_steps)):
-        state = lax.fori_loop(0, n_inner_steps, fori_step_fn, state)
-        trajectory.append(state.position)
+    sample_point = lambda s: lax.fori_loop(0, n_inner_steps, fori_step_fn, s)
+    sample_point = jit(sample_point)
 
-    return trajectory
+    state = deepcopy(init_state)
+    traj = list()
+    for i in tqdm(range(n_outer_steps)):
+        # state = lax.fori_loop(0, n_inner_steps, fori_step_fn, state)
+        state = sample_point(state)
+        traj.append(state.position)
+
+    return tree_stack(traj)
 
 
 def sim_batched_nested_scan(conf_info, top_info, n_inner_steps, n_outer_steps, key, batch_size):
@@ -156,21 +165,28 @@ def sim_batched_nested_scan(conf_info, top_info, n_inner_steps, n_outer_steps, k
     batch_trajs = vmap(batch_fn)(batch_keys)
 
     # FIXME: combine into one big trajectory
-
-    return batch_trajs
+    raise NotImplementedError
+    # return batch_trajs
 
 if __name__ == "__main__":
     import argparse
+    import shutil
 
     parser = argparse.ArgumentParser(description="Simulate 202 bp duplex to eval. memory constraints")
     parser.add_argument('--method', type=str,
                         default="for-loop",
                         choices=["for-loop", "scan", "nested-scan", "nested-for-scan", "batched-nested-scan"],
                         help='Method for simulating')
+    parser.add_argument('--run-name', type=str,
+                        help='Run name')
     parser.add_argument('--n-steps', type=int,
                         help="Number of total steps")
     parser.add_argument('--n-points', type=int,
                         help="Number of total points in the final trajectory")
+    parser.add_argument('--running-avg-interval', type=int,
+                        help="Interval of computed trajectory for computing a running average. Note that this interval is on a trajectory of length n_points, not n_steps")
+    parser.add_argument('--min-running-avg-idx', type=int,
+                        help="Minimum index of the final trajectory for plotting a running average")
     parser.add_argument('--batch-size', type=int, help="Batch size")
 
     args = vars(parser.parse_args())
@@ -178,13 +194,28 @@ if __name__ == "__main__":
     n_steps = args['n_steps']
     n_points = args['n_points']
     batch_size = args['batch_size']
+    run_name = args['run_name']
+    running_avg_interval = args['running_avg_interval']
+    min_running_avg_idx = args['min_running_avg_idx']
 
+    output_dir = Path("output/")
+    run_dir = output_dir / run_name
+    run_dir.mkdir(parents=False, exist_ok=False)
+
+    params_str = ""
+    for k, v in args.items():
+        params_str += f"{k}: {v}\n"
+    with open(run_dir / "params.txt", "w+") as f:
+        f.write(params_str)
+
+    output_path = run_dir / "output.txt"
 
 
     # Load the system
     sys_basedir = Path("data/sys-defs/persistence-length")
     top_path = sys_basedir / "init.top"
     top_info = topology.TopologyInfo(top_path, reverse_direction=True)
+    shutil.copy(top_path, run_dir)
 
     conf_path = sys_basedir / "relaxed.dat"
     conf_info = trajectory.TrajectoryInfo(
@@ -199,13 +230,13 @@ if __name__ == "__main__":
     start = time.time()
 
     if method == "for-loop":
-        trajectory = sim_for_loop(conf_info, top_info, n_steps, key, save_every=sample_every)
+        traj = sim_for_loop(conf_info, top_info, n_steps, key, save_every=sample_every)
     elif method == "scan":
-        trajectory = sim_scan(conf_info, top_info, n_steps, key)
+        traj = sim_scan(conf_info, top_info, n_steps, key, save_every=sample_every)
     elif method == "nested-scan":
-        trajectory = sim_nested_scan(conf_info, top_info, n_inner_steps=sample_every, n_outer_steps=n_points, key=key)
+        traj = sim_nested_scan(conf_info, top_info, n_inner_steps=sample_every, n_outer_steps=n_points, key=key)
     elif method == "nested-for-scan":
-        trajectory = sim_nested_for_scan(conf_info, top_info, n_inner_steps=sample_every, n_outer_steps=n_points, key=key)
+        traj = sim_nested_for_scan(conf_info, top_info, n_inner_steps=sample_every, n_outer_steps=n_points, key=key)
     elif method == "batched-nested-scan":
         assert(n_points % batch_size == 0)
         n_points_per_batch = n_points // batch_size
@@ -214,4 +245,89 @@ if __name__ == "__main__":
         raise RuntimeError(f"Invalid method: {method}")
 
     end = time.time()
-    print(f"Total time: {end - start}")
+
+    with open(output_path, "a") as f:
+        f.write(f"Time to generate trajectory: {end - start} seconds\n")
+
+    traj_info = trajectory.TrajectoryInfo(
+        top_info, read_from_states=True, states=traj, box_size=conf_info.box_size)
+    traj_info.write(run_dir / "traj.dat", reverse=True)
+
+    # Compute the average persistence length
+
+    def get_all_quartets(n_nucs_per_strand):
+        s1_nucs = list(range(n_nucs_per_strand))
+        s2_nucs = list(range(n_nucs_per_strand, n_nucs_per_strand*2))
+        s2_nucs.reverse()
+
+        bps = list(zip(s1_nucs, s2_nucs))
+        n_bps = len(s1_nucs)
+        all_quartets = list()
+        for i in range(n_bps-1):
+            bp1 = bps[i]
+            bp2 = bps[i+1]
+            all_quartets.append(bp1 + bp2)
+        return jnp.array(all_quartets, dtype=jnp.int32)
+
+    quartets = get_all_quartets(n_nucs_per_strand=traj[0].center.shape[0] // 2)
+    quartets = quartets[25:]
+    quartets = quartets[:-25]
+
+
+    all_curves = list()
+    all_l0_avg = list()
+
+    intermediate_lps = dict()
+    assert(isinstance(traj, rigid_body.RigidBody))
+    n_states = traj.center.shape[0]
+
+    base_site = jnp.array([model.com_to_hb, 0.0, 0.0])
+
+    for i in tqdm(range(n_states)):
+        body = traj[i]
+        correlation_curve, l0_avg = persistence_length.get_correlation_curve(body, quartets, base_site)
+        Lp = persistence_length.persistence_length_fit(correlation_curve, l0_avg)
+
+        all_curves.append(correlation_curve)
+        all_l0_avg.append(l0_avg)
+
+        if i % running_avg_interval == 0 and i != 0:
+            mean_correlation_curve = jnp.mean(jnp.array(all_curves), axis=0)
+            mean_Lp = persistence_length.persistence_length_fit(mean_correlation_curve, jnp.mean(jnp.array(all_l0_avg)))
+            intermediate_lps[i*sample_every] = mean_Lp * utils.nm_per_oxdna_length
+
+
+    # Plot the running average
+    plt.plot(intermediate_lps.keys(), intermediate_lps.values())
+    plt.xlabel("Time")
+    plt.ylabel("Lp (nm)")
+    plt.title("Running Average")
+    # plt.show()
+    plt.savefig(run_dir / "running_avg.png")
+    plt.clf()
+
+    # Plot a running average with the first bit cut off
+    plt.plot(list(intermediate_lps.keys())[min_running_avg_idx:], list(intermediate_lps.values())[min_running_avg_idx:])
+    plt.xlabel("Time")
+    plt.ylabel("Lp (nm)")
+    plt.title("Running Average, Iniital Truncation")
+    # plt.show()
+    plt.savefig(run_dir / "truncated_running_avg.png")
+    plt.clf()
+
+
+    # Plot the final correlation curve
+    all_curves = jnp.array(all_curves)
+    all_l0_avg = jnp.array(all_l0_avg)
+    mean_correlation_curve = jnp.mean(all_curves, axis=0)
+    mean_l0_avg = jnp.mean(all_l0_avg)
+
+    plt.plot(mean_correlation_curve)
+    plt.title("Final Correlation Curve")
+    plt.savefig(run_dir / "final_corr_curve.png")
+    # plt.show()
+    plt.clf()
+
+    Lp = persistence_length.persistence_length_fit(mean_correlation_curve, mean_l0_avg)
+    with open(output_path, "a") as f:
+        f.write(f"\nFinal persistence length: {Lp}\n")
