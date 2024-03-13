644a727d12a4e14386edf3d0a2791238acd7c458
Ryan Krueger <ryankrue@umich.edu>
2024-03-13 17:01:48 -0400

    Serial calculation

--- experiments/simulate_tors_mod_const_serial.py
+++ experiments/simulate_tors_mod_const_serial.py
@@ -98,106 +98,17 @@ init_body = init_body.set(center=jnp.array(init_center))
 seq_oh = jnp.array(utils.get_one_hot(top_info.seq), dtype=jnp.float64)
 
 
-
-def sim_torsional(spring_k, key, n_eq_steps, sample_every, n_steps_per_batch, batch_size):
-
-    # Setup our force fn
-    base_energy_fn = lambda body: em.energy_fn(
-        body,
-        seq=seq_oh,
-        bonded_nbrs=top_info.bonded_nbrs,
-        unbonded_nbrs=top_info.unbonded_nbrs.T)
-
-    def harmonic_bias(body):
-        R = body.center
-        bp1_pos = (R[bp1[0]] + R[bp1[1]]) / 2
-        bp2_pos = (R[bp2[0]] + R[bp2[1]]) / 2
-
-        bp1_diff = (bp1_pos - init_bp1)**2
-        bp1_term = bp1_diff.sum()
-
-        # bp2_diff = (bp2_pos - init_bp2)**2
-        bp2_diff = (bp2_pos - init_bp1)**2 # Ensure they're on the same plane
-        bp2_term = bp2_diff[:2].sum()
-
-        # sum_sq_pos = (bp1_pos**2).sum() + (bp2_pos**2).sum()
-        # return 0.5*spring_k*(sum_sq_pos)
-
-        bias_val = bp1_term + bp2_term
-        return 0.5*spring_k*bias_val
-
-    def energy_fn(body, **kwargs):
-        base_energy = base_energy_fn(body, **kwargs)
-        bias_val = harmonic_bias(body)
-        return base_energy + bias_val
-
-
-    # Setup equilibration
-    assert(n_steps_per_batch % sample_every == 0)
-    num_points_per_batch = n_steps_per_batch // sample_every
-    def eq_fn(eq_key):
-        init_fn, step_fn = simulate.nvt_langevin(energy_fn, shift_fn, dt, kT, gamma)
-        init_state = init_fn(eq_key, init_body, mass=mass)
-        def fori_step_fn(t, state):
-            return step_fn(state)
-        fori_step_fn = jit(fori_step_fn)
-
-        eq_state = lax.fori_loop(0, n_eq_steps, fori_step_fn, init_state)
-        return eq_state.position
-
-    # Setup sampling simulation
-    def batch_fn(batch_key, eq_body):
-        init_fn, step_fn = simulate.nvt_langevin(energy_fn, shift_fn, dt, kT, gamma)
-        init_state = init_fn(batch_key, eq_body, mass=mass)
-
-        def fori_step_fn(t, state):
-            return step_fn(state)
-        fori_step_fn = jit(fori_step_fn)
-
-        @jit
-        def scan_fn(state, step):
-            state = lax.fori_loop(0, sample_every, fori_step_fn, state)
-            return state, state.position
-
-        _, traj = lax.scan(scan_fn, init_state, jnp.arange(num_points_per_batch))
-        return traj
-
-    # Equilibrate and sample
-    key, eq_key = random.split(key)
-    eq_keys = random.split(eq_key, batch_size)
-    eq_bodies = vmap(eq_fn)(eq_keys)
-
-    batch_keys = random.split(key, batch_size)
-    batch_trajs = vmap(batch_fn)(batch_keys, eq_bodies)
-
-    num_bases = batch_trajs.center.shape[2]
-    assert(batch_trajs.center.shape[3] == 3)
-
-    combined_center = batch_trajs.center.reshape(-1, num_bases, 3)
-    combined_quat_vec = batch_trajs.orientation.vec.reshape(-1, num_bases, 4)
-
-    combined_traj = rigid_body.RigidBody(
-        center=combined_center,
-        orientation=rigid_body.Quaternion(combined_quat_vec))
-
-    return combined_traj
-
-
 def run(args):
 
     # Load arguments
     key_seed = args['key_seed']
     key = random.PRNGKey(key_seed)
-    n_steps_per_batch = args['n_steps_per_batch']
+    n_sample_steps = args['n_sample_steps']
     n_eq_steps = args['n_eq_steps']
-    batch_size = args['n_sims']
     sample_every = args['sample_every']
     run_name = args['run_name']
-    assert(n_steps_per_batch % sample_every == 0)
-    num_points_per_batch = n_steps_per_batch // sample_every
-    num_points = num_points_per_batch * batch_size
+    assert(n_sample_steps % sample_every == 0)
     spring_k = args['spring_k']
-    n_sims = args['n_sims']
 
 
     # Setup the logging directoroy
@@ -207,6 +118,8 @@ def run(args):
     run_dir = output_dir / run_name
     run_dir.mkdir(parents=False, exist_ok=False)
 
+    shutil.copy(top_path, run_dir / "sys.top")
+
     params_str = ""
     for k, v in args.items():
         params_str += f"{k}: {v}\n"
@@ -214,22 +127,49 @@ def run(args):
         f.write(params_str)
 
 
-    f_key, key = random.split(key)
     start = time.time()
-    traj = sim_torsional(spring_k, f_key, n_eq_steps, sample_every,
-                         n_steps_per_batch, batch_size=n_sims)
+    base_energy_fn = lambda body: em.energy_fn(
+        body,
+        seq=seq_oh,
+        bonded_nbrs=top_info.bonded_nbrs,
+        unbonded_nbrs=top_info.unbonded_nbrs.T)
+    def energy_fn(body, **kwargs):
+        base_energy = base_energy_fn(body, **kwargs)
+        # bias_val = harmonic_bias(body)
+        # return base_energy + bias_val
+        return base_energy
+
+    init_fn, step_fn = simulate.nvt_langevin(energy_fn, shift_fn, dt, kT, gamma)
+    key, eq_key = random.split(key)
+    state = init_fn(eq_key, init_body, mass=mass)
+    eq_traj = list()
+    for i in tqdm(range(n_eq_steps), colour="blue", desc="Equilibrating"):
+        state = step_fn(state)
+        if i % sample_every == 0:
+            eq_traj.append(state.position)
+    eq_traj = utils.tree_stack(eq_traj)
+
+    traj = list()
+    for i in tqdm(range(n_sample_steps), colour="green", desc="Sampling"):
+        state = step_fn(state)
+        if i % sample_every == 0:
+            traj.append(state.position)
+    traj = utils.tree_stack(traj)
+
     end = time.time()
     print(f"- Simulation finished: {end - start} seconds\n")
 
 
     # Write all the trajectories (after we've done relevant analysis)
+    eq_traj_info = trajectory.TrajectoryInfo(
+        top_info, read_from_states=True, states=eq_traj, box_size=conf_info.box_size)
+    eq_traj_info.write(run_dir / f"eq_traj.dat", reverse=True)
+
     traj_info = trajectory.TrajectoryInfo(
         top_info, read_from_states=True, states=traj, box_size=conf_info.box_size)
     traj_info.write(run_dir / f"traj.dat", reverse=True)
-    end = time.time()
 
     # Compute the torsional modulus
-
     def compute_theta(body):
         pitches = pitch.get_all_pitches(body, quartets, displacement_fn, model.com_to_hb)
         return pitches.sum()
@@ -324,9 +264,7 @@ def run(args):
 def get_parser():
     parser = argparse.ArgumentParser(description="Simulate torsional modulus with x- and y- constrained")
     parser.add_argument('--run-name', type=str, help='Run name')
-    parser.add_argument('--n-sims', type=int, default=2,
-                        help="Number of simulations.")
-    parser.add_argument('--n-steps-per-batch', type=int, default=int(1e7),
+    parser.add_argument('--n-sample-steps', type=int, default=int(1e7),
                         help="Number of total steps (post-equilibration) for sampling per batch.")
     parser.add_argument('--n-eq-steps', type=int, help="Number of equilibration steps")
     parser.add_argument('--sample-every', type=int, default=int(1e4),
