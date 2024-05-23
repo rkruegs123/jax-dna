2a561dbfb228e2d5651bf96ecc8bd59862f4551f
Ryan Hausen <ryan.hausen@gmail.com>
2024-05-22 20:20:04 -0400

    lint/format fix

--- jax_dna/__init__.py
+++ jax_dna/__init__.py
@@ -1,7 +1,8 @@
 """jax_dna is tool to simulate DNA/RNA systems using JAX."""
+
 __project__ = "jax_dna"
 
 try:
     from ._version import __version__
 except ImportError:
-    __version__ = ''
+    __version__ = ""
