c3cff91c5a43efc6f05a01c09af71b06abff27d8
Ryan Hausen <ryan.hausen@gmail.com>
2024-06-05 21:20:28 -0400

    wip strategy sketch

---
+++ jax_dna/strategies/direct.py
@@ -0,0 +1,16 @@
+
+from typing import Any
+
+import jax
+import jax.numpy as jnp
+
+def optimize_structure(
+    input_config: dict[str, Any],
+) -> jnp.ndarray:
+    pass
+
+def optimize_energy_params(
+    params: dict[str, Any],
+    input_config: dict[str, Any],
+) -> dict[str, Any]:
+    pass
