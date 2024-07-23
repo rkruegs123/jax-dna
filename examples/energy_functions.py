ad72154404dfc46c9505e17fc789a7c513c3cfbe
Ryan Hausen <ryan.hausen@gmail.com>
2024-07-23 15:21:18 -0400

    updated energy function example to reflect changes in energy/config prototypes

--- examples/energy_functions.py
+++ examples/energy_functions.py
@@ -10,22 +10,30 @@ import jax_dna.input.dna1.bonded as  dna1_bonded_config
 import jax_dna.input.dna1.unbonded as dna1_unbonded_config
 import jax_dna.energy.dna1 as dna1_energy
 import jax_dna.energy.base as jdna_energy
+import jax_dna.utils.types as jdt
 
 jax.config.update("jax_enable_x64", True)
 
 if __name__=="__main__":
 
+    topology_fname = "data/templates/simple-helix/sys.top"
+    traj_fname = "data/templates/simple-helix/init.conf"
+    config_fname = "jax_dna/input/dna1/defaults.toml"
+
+
+
     displacement_fn, shift_fn = jax_md.space.free()
 
-    top = topology.from_oxdna_file("data/templates/simple-helix/sys.top")
+    top = topology.from_oxdna_file(topology_fname)
     seq = jnp.array(top.seq_one_hot)
     traj = trajectory.from_file(
-        "data/templates/simple-helix/init.conf",
+        traj_fname,
         top.strand_counts,
     )
+    experiment_config = config.BaseConfiguration.parse_toml(config_fname)
 
-    dt = 5e-3
-    kT = 296.15 * 0.1 / 300.0
+    dt = experiment_config["dt"]
+    kT = experiment_config["t_kelvin"]
 
     gamma = jax_md.rigid_body.RigidBody(
         center=jnp.array([kT/2.5], dtype=jnp.float64),
@@ -49,61 +57,52 @@ if __name__=="__main__":
 
     transform_fn = functools.partial(
         dna1_energy.Nucleotide.from_rigid_body,
-        com_to_backbone=-0.4,
-        com_to_hb=0.4,
-        com_to_stacking=0.34,
+        com_to_backbone=experiment_config["com_to_backbone"],
+        com_to_hb=experiment_config["com_to_hb"],
+        com_to_stacking=experiment_config["com_to_stacking"],
     )
 
 
     config_file = "jax_dna/input/dna1/defaults.toml"
     configs = [
-        dna1_bonded_config.FeneConfiguration.from_toml(config_file),
-        dna1_bonded_config.ExcludedVolumeConfiguration,
-        dna1_bonded_config.StackingConfiguration,
-        dna1_unbonded_config.ExcludedVolumeConfiguration,
-        dna1_unbonded_config.HydrogenBondingConfiguration,
-        dna1_unbonded_config.CrossStackingConfiguration,
-        dna1_unbonded_config.CoaxialStackingConfiguration,
-    ]
-
-
-    opt_params = [
-        dna1_bonded_config.VFeneConfiguration(eps_backbone=0.15),
-        dna1_bonded_config.ExcludedVolumeConfiguration(
-            eps_exc=1.0,
-            sigma_base=1.0,
-            sigma_back_base=1.0,
-        ),
+        # single param
+        dna1_bonded_config.FeneConfiguration.from_toml(config_file, ("eps_backbone",)),
+        # multiple params
+        dna1_bonded_config.ExcludedVolumeConfiguration.from_toml(config_file, ("eps_exc","dr_star_base")),
+        # shortcut for all params, though you could list them all too
+        dna1_bonded_config.StackingConfiguration.from_toml(config_file, ("*",)),
+        dna1_unbonded_config.ExcludedVolumeConfiguration.from_toml(config_file),
+        dna1_unbonded_config.HydrogenBondingConfiguration.from_toml(config_file),
+        dna1_unbonded_config.CrossStackingConfiguration.from_toml(config_file),
+        dna1_unbonded_config.CoaxialStackingConfiguration.from_toml(config_file),
     ]
 
 
 
+    opt_params = [c.opt_params for c in configs]
+
+    # configs and energy functions should be in the same order
+    # though maybe we can be more clever in the future
     energy_fns = [
         dna1_energy.bonded.Fene,
         dna1_energy.bonded.ExcludedVolume,
         dna1_energy.bonded.Stacking,
         dna1_energy.unbonded.ExcludedVolume,
         dna1_energy.unbonded.HydrogenBonding,
-        dna1_energy.unbonded.CrossStacking,
-        dna1_energy.unbonded.CoaxialStacking,
+        # dna1_energy.unbonded.CrossStacking,
+        # dna1_energy.unbonded.CoaxialStacking,
     ]
 
-
-
-
-
-
-
     def test_energy_fn(
-        opt_params: list[config.BaseConfiguration],
+        params: list[dict[str, jdt.ARR_OR_SCALAR]],
     ) -> float:
 
         transformed_fns = [
             energy_fn(
                 displacement_fn=displacement_fn,
-                params=(param | opt_param).init_params(),
+                params=(config | param).init_params(),
             )
-            for opt_param, param, energy_fn in zip(opt_params, params, energy_fns)
+            for param, config, energy_fn in zip(params, configs, energy_fns)
         ]
 
         energy_fn = jdna_energy.ComposedEnergyFunction(
@@ -122,14 +121,14 @@ if __name__=="__main__":
 
 
     def test_energy_grad_fn(
-        opt_params: list[config.BaseConfiguration],
+        params: list[config.BaseConfiguration],
     ) -> float:
         transformed_fns = [
             energy_fn(
                 displacement_fn=displacement_fn,
-                params=(param | opt_param).init_params(),
+                params=(config | param).init_params(),
             )
-            for opt_param, param, energy_fn in zip(opt_params, params, energy_fns)
+            for param, config, energy_fn in zip(params, configs, energy_fns)
         ]
 
         energy_fn = jdna_energy.ComposedEnergyFunction(
