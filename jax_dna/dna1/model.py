9794ed608ecc5d831e75bf52ce783151e7ff9c53
Ryan Krueger <ryankrue@umich.edu>
2024-08-07 13:31:28 -0400

    Changing def of theta8 OP for dna1 and rna2 as well. Cautiously.

--- jax_dna/dna1/model.py
+++ jax_dna/dna1/model.py
@@ -147,7 +147,8 @@ class EnergyModel:
         theta4_op = jnp.arccos(clamp(jnp.einsum('ij, ij->i', base_normals[op_i], base_normals[op_j])))
         # note: are these swapped in Lorenzo's code?
         theta7_op = jnp.arccos(clamp(jnp.einsum('ij, ij->i', -base_normals[op_j], dr_base_op) / r_base_op))
-        theta8_op = jnp.pi - jnp.arccos(clamp(jnp.einsum('ij, ij->i', base_normals[op_i], dr_base_op) / r_base_op))
+        # theta8_op = jnp.pi - jnp.arccos(clamp(jnp.einsum('ij, ij->i', base_normals[op_i], dr_base_op) / r_base_op))
+        theta8_op = jnp.arccos(clamp(jnp.einsum('ij, ij->i', base_normals[op_i], dr_base_op) / r_base_op))
 
         ## Cross stacking variables -- all already computed
 
