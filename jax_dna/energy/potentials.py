01a43e398a1f352a2aefdf91c661b10089d8c6c5
Ryan Hausen <ryan.hausen@gmail.com>
2024-08-07 16:57:35 -0400

    wip potential function tests

--- jax_dna/energy/potentials.py
+++ jax_dna/energy/potentials.py
@@ -1,24 +1,43 @@
 import jax.numpy as jnp
 
+import jax_dna.utils.types as jd_types
+
 # https://ora.ox.ac.uk/objects/uuid:b2415bb2-7975-4f59-b5e2-8c022b4a3719/files/mdcac62bc9133143fc05070ed20048c50
 # Section 2.4.1
 
 
-def v_fene(r, eps, r0, delt):
-    """
-    The vanilla FENE potential. Used in a smoothed FENE potential in oxDNA.
+def v_fene(r: jd_types.ARR_OR_SCALAR, eps: float, r0: float, delt: float) -> jd_types.ARR_OR_SCALAR:
+    """The FENE potential.
+
+    This is based on equation 2.1 from the oxDNA paper.
     """
     x = (r - r0) ** 2 / delt**2
-    # Note: if `x` is too big, we will easily try to take the log of negatives, which will yield `nan`
     return -eps / 2.0 * jnp.log(1 - x)
 
 
-def v_morse(r, eps, r0, a):
+def v_morse(
+    r: jd_types.ARR_OR_SCALAR,
+    eps: float,
+    r0: float,
+    a: float,
+) -> jd_types.ARR_OR_SCALAR:
+    """The Morse potential.
+
+    This is based on equation 2.2 from the oxDNA paper.
+    """
     x = -(r - r0) * a
     return eps * (1 - jnp.exp(x)) ** 2
 
 
-def v_harmonic(r, k, r0):
+def v_harmonic(
+    r: jd_types.ARR_OR_SCALAR,
+    k: float,
+    r0: float,
+) -> jd_types.ARR_OR_SCALAR:
+    """The Harmonic potential.
+
+    This is based on equation 2.3 from the oxDNA paper.
+    """
     return k / 2 * (r - r0) ** 2
 
 
---
+++ jax_dna/energy/tests/test_potentials.py
@@ -0,0 +1,84 @@
+import jax
+import numpy as np
+import pytest
+import sympy as sp
+
+import jax_dna.energy.potentials as potentials
+
+jax.config.update("jax_enable_x64", True)
+
+# functional forms from oxDNA paper
+# https://ora.ox.ac.uk/objects/uuid:b2415bb2-7975-4f59-b5e2-8c022b4a3719/files/mdcac62bc9133143fc05070ed20048c50
+# Section 2.4.1
+
+
+def _sympy_v_fene() -> sp.Expr:
+    """String form of Equation 2.1 from the oxDNA paper."""
+    return sp.parsing.sympy_parser.parse_expr("-eps / 2 * log(1 - (r-r0)**2 / delt**2)")
+
+
+@pytest.mark.parametrize(
+    "r, eps, r0, delt",
+    [
+        (5, 3, 2, 10),
+    ],
+)
+def test_v_fene(r: float, eps: float, r0: float, delt: float):
+    """tests v_fene potential. Function is equation 2.1 form the oxDNA paper."""
+    actual = float(potentials.v_fene(r, eps, r0, delt))
+    expected = float(_sympy_v_fene().evalf(subs={"r": r, "eps": eps, "r0": r0, "delt": delt}))
+    np.testing.assert_allclose(actual, expected)
+
+
+def _sympy_v_morse() -> sp.Expr:
+    """String form of Equation 2.2 from the oxDNA paper."""
+    return sp.parsing.sympy_parser.parse_expr("eps * (1 - exp(-(r-r0)*a))**2")
+
+
+@pytest.mark.parametrize(
+    "r, eps, r0, a",
+    [
+        (5, 3, 2, 10),
+    ],
+)
+def test_v_morse(r: float, eps: float, r0: float, a: float):
+    """tests v_morse potential. Function is equation 2.2 form the oxDNA paper."""
+    actual = float(potentials.v_morse(r, eps, r0, a))
+    expected = float(_sympy_v_morse().evalf(subs={"r": r, "eps": eps, "r0": r0, "a": a}))
+    np.testing.assert_allclose(actual, expected)
+
+
+def _sympy_v_harmonic() -> sp.Expr:
+    """String form of Equation 2.3 from the oxDNA paper."""
+    return sp.parsing.sympy_parser.parse_expr("k / 2 * (r - r0)**2")
+
+
+@pytest.mark.parametrize(
+    "r, k, r0",
+    [
+        (5, 3, 2),
+    ],
+)
+def test_v_harmonic(r: float, k: float, r0: float):
+    """tests v_harmonic potential. Function is equation 2.3 form the oxDNA paper."""
+    actual = float(potentials.v_harmonic(r, k, r0))
+    expected = float(_sympy_v_harmonic().evalf(subs={"r": r, "k": k, "r0": r0}))
+    np.testing.assert_allclose(actual, expected)
+
+
+def _sympy_harmonic() -> sp.Expr:
+    """String form of Equation 2.3 from the oxDNA paper."""
+    return sp.parsing.sympy_parser.parse_expr("k / 2 * (r - r0)**2")
+
+
+@pytest.mark.parametrize(
+    "r, k, r0",
+    [
+        (5, 3, 2),
+    ],
+)
+def test_harmonic(r: float, k: float, r0: float):
+    """tests harmonic potential. Function is equation 2.3 form the oxDNA paper."""
+    actual = float(potentials.v_harmonic(r, k, r0))
+    expected = float(_sympy_harmonic().evalf(subs={"r": r, "k": k, "r0": r0}))
+    np.testing.assert_allclose(actual, expected)
