3d14c1a968359fbc0b358fecbccffc0fa9571b8a
Ryan Krueger <ryankrue@umich.edu>
2024-09-01 14:42:08 -0400

    Plotting a bunch of stuff

---
+++ figures/fig4/plot_joint_c_seff.py
@@ -0,0 +1,76 @@
+import numpy as onp
+from pathlib import Path
+import pdb
+
+from matplotlib import rc
+from matplotlib.colors import LinearSegmentedColormap
+import matplotlib.pyplot as plt
+
+from figures.utils import colors, labels
+
+
+font = {'size': 48}
+rc('font', **font)
+rc('text', usetex=True)
+
+
+data_dir = Path("figures/fig4/data")
+joint_dir = data_dir / "moduli/c-seff-joint"
+output_dir = Path("figures/fig4/output")
+
+max_iter = 300
+
+for width, height in [(20, 14), (24, 14), (28, 14)]:
+
+    # fig, ax = plt.subplots(figsize=(width, height))
+    fig, (ax1, ax2) = plt.subplots(2, 1, height_ratios=[1, 1], figsize=(width, height))
+
+
+    fin_cs_path = joint_dir / "obj/fin_cs.npy"
+    fin_cs = onp.load(fin_cs_path)[:max_iter]
+
+    fin_seffs_path = joint_dir / "obj/fin_seffs.npy"
+    fin_seffs = onp.load(fin_seffs_path)[:max_iter]
+
+    fin_ref_iters_path = joint_dir / "obj/fin_ref_iters.npy"
+    fin_ref_iters = onp.load(fin_ref_iters_path)
+
+    fin_ref_cs_path = joint_dir / "obj/fin_ref_cs.npy"
+    fin_ref_cs = onp.load(fin_ref_cs_path)
+
+    fin_ref_seffs_path = joint_dir / "obj/fin_ref_seffs.npy"
+    fin_ref_seffs = onp.load(fin_ref_seffs_path)
+
+    keep_ref_iters = fin_ref_iters < max_iter
+    fin_ref_iters = fin_ref_iters[keep_ref_iters]
+    fin_ref_cs = fin_ref_cs[keep_ref_iters]
+    fin_ref_seffs = fin_ref_seffs[keep_ref_iters]
+
+    ax1.plot(fin_cs, color="black", linewidth=3)
+    ax1.scatter(fin_ref_iters, fin_ref_cs, color="black", s=100)
+
+    ax1.fill_between(onp.arange(len(fin_cs)), 436-16, 436+16, color='green', alpha=0.3, label="Experimental Uncertainty", transform=ax1.get_yaxis_transform())
+
+    ax2.plot(fin_seffs, color="black", linewidth=3)
+    ax2.scatter(fin_ref_iters, fin_ref_seffs, color="black", s=100)
+    ax2.fill_between(onp.arange(len(fin_seffs)), 1045-92, 1045+92, color='green', alpha=0.3, label="Experimental Uncertainty", transform=ax2.get_yaxis_transform())
+
+    ax2.set_xlabel("Iteration")
+    ax1.set_ylabel('c ($pN·nm^2$)', usetex=False)
+    ax2.set_ylabel('$\mathrm{S_{eff}}$ ($pN$)', usetex=False)
+    # ax.set_ylabel('c (pN·nm2)')
+
+    # y_vals = [10.5, 11, 11.5, 12]
+    # ax.set_yticks(y_vals)
+    # ax.set_yticklabels([r'$10.5$', r'$11$', r'$11.5$', r'$12$'])
+
+    # x_vals = [0, 25, 50]
+    # ax.set_xticks(x_vals)
+    # ax.set_xticklabels([r'$0$', r'$25$', r'$50$'])
+    # ax.set_ylim(top=45)
+
+    leg = ax1.legend(prop={'size': 48})
+
+    # plt.show()
+    # plt.savefig(output_dir / f"joint_{width}x{height}.pdf")
+    plt.savefig(output_dir / f"joint_{width}x{height}.svg")
