be5edadd9eb38e3a5c82dacedc9606b8513a1354
Ryan Krueger <ryankrue@umich.edu>
2024-09-17 12:41:10 -0400

    Trying to use batch sim and trying to change to e2e dist

--- experiments/seq_opt_e2e_dist.py
+++ experiments/seq_opt_e2e_dist.py
@@ -16,7 +16,6 @@ from jax import jit, vmap, random, grad, value_and_grad, lax, lax
 from jax_md import space, simulate, rigid_body
 
 from jax_dna.common import utils, topology, trajectory, checkpoint
-from jax_dna.loss import geometry, pitch, propeller
 from jax_dna.dna1 import model
 from jax_dna.common.read_seq_specific import read_ss_oxdna
 
@@ -46,11 +45,12 @@ def run(args):
     lr = args['lr']
     min_neff_factor = args['min_neff_factor']
     n_sample_steps = args['n_sample_steps']
+    n_sims = args['n_sims']
     assert(n_sample_steps % sample_every == 0)
     n_ref_states = n_sample_steps // sample_every
     plot_every = args['plot_every']
     run_name = args['run_name']
-    target_ptwist = args['target_ptwist']
+    target_dist = args['target_dist']
     max_approx_iters = args['max_approx_iters']
 
     # Setup the logging directoroy
@@ -82,7 +82,7 @@ def run(args):
 
 
     # Load the system
-    sys_basedir = Path("data/sys-defs/simple-helix")
+    sys_basedir = Path("data/templates/ss20")
     top_path = sys_basedir / "sys.top"
     top_info = topology.TopologyInfo(top_path, reverse_direction=True)
     seq_length = len(top_info.seq)
@@ -95,7 +95,7 @@ def run(args):
 
         return pseq
 
-    conf_path = sys_basedir / "bound_relaxed.conf"
+    conf_path = sys_basedir / "init.conf"
     conf_info = trajectory.TrajectoryInfo(
         top_info,
         read_from_file=True, traj_path=conf_path, reverse_direction=True
@@ -115,8 +115,25 @@ def run(args):
                                 orientation=jnp.array([utils.moment_of_inertia], dtype=jnp.float64))
 
 
-    def sim_fn(params, body, n_steps, key, pseq):
-        em = model.EnergyModel(displacement_fn, empty_params, t_kelvin=t_kelvin, ss_hb_weights=ss_hb_weights, ss_stack_weights=ss_stack_weights)
+    em = model.EnergyModel(displacement_fn, empty_params, t_kelvin=t_kelvin, ss_hb_weights=ss_hb_weights, ss_stack_weights=ss_stack_weights)
+
+    @jit
+    def eq_fn(eq_key, body, pseq):
+        init_fn, step_fn = simulate.nvt_langevin(em.energy_fn, shift_fn, dt, kT, gamma)
+        init_state = init_fn(key, body, mass=mass, seq=pseq,
+                             bonded_nbrs=top_info.bonded_nbrs,
+                             unbonded_nbrs=top_info.unbonded_nbrs.T)
+        def fori_step_fn(t, state):
+            return state = step_fn(state,
+                                   seq=pseq,
+                                   bonded_nbrs=top_info.bonded_nbrs,
+                                   unbonded_nbrs=top_info.unbonded_nbrs.T)
+        fori_step_fn = jit(fori_step_fn)
+
+        eq_state = lax.fori_loop(0, n_eq_steps, fori_step_fn, init_state)
+        return eq_state.position
+
+    def sample_fn(body, key, pseq):
         init_fn, step_fn = simulate.nvt_langevin(em.energy_fn, shift_fn, dt, kT, gamma)
         init_state = init_fn(key, body, mass=mass, seq=pseq,
                              bonded_nbrs=top_info.bonded_nbrs,
@@ -132,13 +149,28 @@ def run(args):
             return state, state.position
 
         start = time.time()
-        fin_state, traj = scan(scan_fn, init_state, jnp.arange(n_steps))
+        fin_state, traj = scan(scan_fn, init_state, jnp.arange(n_sample_steps))
         end = time.time()
         print(f"Generating reference states took {end - start} seconds")
 
-
         return traj
 
+    @jit
+    def batch_sim(ref_key, R, pseq):
+
+        ref_key, eq_key = random.split(ref_key)
+        eq_keys = random.split(eq_key, n_sims)
+        eq_states = vmap(eq_fn, (0, None, None, None))(eq_keys, R, pseq, mass)
+
+        sample_keys = random.split(ref_key, n_sims)
+        sample_trajs = vmap(sample_fn, (0, 0, None))(eq_states, sample_keys, pseq)
+
+        sample_traj = utils.tree_stack(sample_traj)
+        return sample_traj
+
+
+    def e2e_distance(body):
+        return space.distance(displacement_fn(body.center[0], body.center[-1]))
 
     def get_ref_states(params, init_body, key, i, temp):
 
@@ -149,12 +181,9 @@ def run(args):
         iter_dir = ref_traj_dir / f"iter{i}"
         iter_dir.mkdir(parents=False, exist_ok=False)
 
+        iter_key, batch_key = random.split(iter_key)
+        ref_states = batch_sim(batch_key, init_body, pseq):
 
-        trajectory = sim_fn(empty_params, init_body, n_eq_steps + n_sample_steps, key, pseq)
-        eq_trajectory = trajectory[n_eq_steps:]
-        ref_states = eq_trajectory[::sample_every]
-
-        em = model.EnergyModel(displacement_fn, empty_params, t_kelvin=t_kelvin, ss_hb_weights=ss_hb_weights, ss_stack_weights=ss_stack_weights)
         energy_fn = lambda body: em.energy_fn(body,
                                               seq=pseq,
                                               bonded_nbrs=top_info.bonded_nbrs,
@@ -166,48 +195,26 @@ def run(args):
 
         ref_energies = vmap(energy_fn)(ref_states)
 
-        ref_ptwists = vmap(compute_avg_ptwist)(ref_states) # FIXME: this doesn't depend on params...
+        ref_dists = vmap(e2e_distance)(ref_states) # FIXME: this doesn't depend on params...
 
-        n_traj_states = len(ref_ptwists)
-        running_avg_ptwists = onp.cumsum(ref_ptwists) / onp.arange(1, n_traj_states + 1)
-        plt.plot(running_avg_ptwists)
+        n_traj_states = len(ref_dists)
+        running_avg_dists = onp.cumsum(ref_dists) / onp.arange(1, n_traj_states + 1)
+        plt.plot(running_avg_dists)
         plt.savefig(iter_dir / f"running_avg.png")
         plt.close()
 
-        plt.plot(running_avg_ptwists[-int(n_traj_states // 2):])
+        plt.plot(running_avg_dists[-int(n_traj_states // 2):])
         plt.savefig(iter_dir / f"running_avg_second_half.png")
         plt.close()
 
-        return ref_states, ref_energies, ref_ptwists
-
-
-    # Construct the loss function terms
-
-    ## note: we don't include the end base pairs due to fraying
-    compute_helical_diameters, helical_diam_loss_fn = geometry.get_helical_diameter_loss_fn(
-        top_info.bonded_nbrs[1:-1], displacement_fn, model.com_to_backbone)
-
-    compute_bb_distances, bb_dist_loss_fn = geometry.get_backbone_distance_loss_fn(
-        top_info.bonded_nbrs, displacement_fn, model.com_to_backbone)
-
-    simple_helix_quartets = jnp.array([
-        [1, 14, 2, 13], [2, 13, 3, 12],
-        [3, 12, 4, 11], [4, 11, 5, 10],
-        [5, 10, 6, 9]])
-    compute_avg_pitch, pitch_loss_fn = pitch.get_pitch_loss_fn(
-        simple_helix_quartets, displacement_fn, model.com_to_hb)
-
-    simple_helix_bps = jnp.array([[1, 14], [2, 13], [3, 12],
-                                  [4, 11], [5, 10], [6, 9]])
-    compute_avg_ptwist, ptwist_loss_fn = propeller.get_propeller_loss_fn(simple_helix_bps)
+        return ref_states, ref_energies, ref_dists
 
 
     @jit
-    def loss_fn(params, ref_states: rigid_body.RigidBody, ref_energies, ref_ptwists, temp):
+    def loss_fn(params, ref_states: rigid_body.RigidBody, ref_energies, ref_dists, temp):
         logits = params['logits']
         pseq = normalize(logits, temp)
 
-        em = model.EnergyModel(displacement_fn, empty_params, t_kelvin=t_kelvin, ss_hb_weights=ss_hb_weights, ss_stack_weights=ss_stack_weights)
 
         # Compute the weights
         energy_fn = lambda body: em.energy_fn(body,
@@ -222,15 +229,15 @@ def run(args):
         weights = boltzs / denom
 
         # Compute the observable
-        expected_ptwist = jnp.dot(weights, ref_ptwists)
+        expected_dist = jnp.dot(weights, ref_dists)
 
         # Compute effective sample size
         n_eff = jnp.exp(-jnp.sum(weights * jnp.log(weights)))
 
-        mse = (expected_ptwist - target_ptwist)**2
+        mse = (expected_dist - target_dist)**2
         rmse = jnp.sqrt(mse)
 
-        return rmse, (n_eff, expected_ptwist)
+        return rmse, (n_eff, expected_dist)
     grad_fn = value_and_grad(loss_fn, has_aux=True)
     grad_fn = jit(grad_fn)
 
@@ -245,51 +252,51 @@ def run(args):
 
     init_body = conf_info.get_states()[0]
     print(f"Generating initial reference states and energies...")
-    ref_states, ref_energies, ref_ptwists = get_ref_states(params, init_body, key, 0, temp=gumbel_temps[0])
+    ref_states, ref_energies, ref_dists = get_ref_states(params, init_body, key, 0, temp=gumbel_temps[0])
 
     min_n_eff = int(n_ref_states * min_neff_factor)
     all_losses = list()
-    all_eptwists = list()
+    all_edists = list()
     all_ref_losses = list()
-    all_ref_eptwists = list()
+    all_ref_edists = list()
     all_ref_times = list()
 
     loss_path = log_dir / "loss.txt"
     neff_path = log_dir / "neff.txt"
-    ptwist_path = log_dir / "ptwist.txt"
+    dist_path = log_dir / "dist.txt"
 
     num_resample_iters = 0
     for i in tqdm(range(n_iters)):
-        (loss, (n_eff, expected_ptwist)), grads = grad_fn(params, ref_states, ref_energies, ref_ptwists, gumbel_temps[0])
+        (loss, (n_eff, expected_dist)), grads = grad_fn(params, ref_states, ref_energies, ref_dists, gumbel_temps[0])
 
         pdb.set_trace()
 
         if i == 0:
             all_ref_losses.append(loss)
             all_ref_times.append(i)
-            all_ref_eptwists.append(expected_ptwist)
+            all_ref_edists.append(expected_dist)
 
         if n_eff < min_n_eff or num_resample_iters >= max_approx_iters:
             num_resample_iters
 
             print(f"Resampling reference states...")
             key, split = random.split(key)
-            # ref_states, ref_energies, ref_ptwists = get_ref_states(params, ref_states[-1], split, i, temp=gumbel_temps[i])
-            ref_states, ref_energies, ref_ptwists = get_ref_states(params, init_body, split, i, temp=gumbel_temps[i])
-            (loss, (n_eff, expected_ptwist)), grads = grad_fn(params, ref_states, ref_energies, ref_ptwists, gumbel_temps[i])
+            # ref_states, ref_energies, ref_dists = get_ref_states(params, ref_states[-1], split, i, temp=gumbel_temps[i])
+            ref_states, ref_energies, ref_dists = get_ref_states(params, init_body, split, i, temp=gumbel_temps[i])
+            (loss, (n_eff, expected_dist)), grads = grad_fn(params, ref_states, ref_energies, ref_dists, gumbel_temps[i])
 
             all_ref_losses.append(loss)
-            all_ref_eptwists.append(expected_ptwist)
+            all_ref_edists.append(expected_dist)
             all_ref_times.append(i)
 
         with open(loss_path, "a") as f:
             f.write(f"{loss}\n")
-        with open(ptwist_path, "a") as f:
-            f.write(f"{expected_ptwist}\n")
+        with open(dist_path, "a") as f:
+            f.write(f"{expected_dist}\n")
         with open(neff_path, "a") as f:
             f.write(f"{n_eff}\n")
         all_losses.append(loss)
-        all_eptwists.append(expected_ptwist)
+        all_edists.append(expected_dist)
 
 
         print(f"Loss: {loss}")
@@ -305,19 +312,19 @@ def run(args):
             plt.xlabel("Iteration")
             plt.ylabel("Loss")
             plt.legend()
-            plt.title(f"DiffTRE Propeller Twist Optimization, Neff factor={min_neff_factor}")
+            # plt.title(f"DiffTRE E2E Dist Optimization, Neff factor={min_neff_factor}")
             plt.savefig(img_dir / f"losses_iter{i}.png")
             plt.clf()
 
             # Plot the persistence lengths
-            plt.plot(onp.arange(i+1), all_eptwists, linestyle="--", color='blue')
-            plt.scatter(all_ref_times, all_ref_eptwists, marker='o', label="Resample points", color='blue')
-            plt.axhline(y=target_ptwist, linestyle='--', label="Target p. twist", color='red')
+            plt.plot(onp.arange(i+1), all_edists, linestyle="--", color='blue')
+            plt.scatter(all_ref_times, all_ref_edists, marker='o', label="Resample points", color='blue')
+            plt.axhline(y=target_dist, linestyle='--', label="Target E2E dist", color='red')
             plt.xlabel("Iteration")
-            plt.ylabel("Expected Propeller Twist (deg)")
+            plt.ylabel("Expected E2E Dist (oxDNA units)")
             plt.legend()
-            plt.title(f"DiffTRE Propeller Twist Optimization, Neff factor={min_neff_factor}")
-            plt.savefig(img_dir / f"eptwists_iter{i}.png")
+            # plt.title(f"DiffTRE E2E Dist Optimization, Neff factor={min_neff_factor}")
+            plt.savefig(img_dir / f"edists_iter{i}.png")
             plt.clf()
 
 def get_parser():
@@ -329,6 +336,8 @@ def get_parser():
                         help="Number of equilibration steps")
     parser.add_argument('--n-sample-steps', type=int, default=100000,
                         help="Number of total steps for sampling reference states")
+    parser.add_argument('--n-sims', type=int, default=5,
+                        help="Number of independent simulations")
     parser.add_argument('--sample-every', type=int, default=500,
                         help="Frequency of sampling reference states.")
     parser.add_argument('--plot-every', type=int, default=10,
@@ -337,8 +346,8 @@ def get_parser():
                         help="Learning rate")
     parser.add_argument('--min-neff-factor', type=float, default=0.95,
                         help="Factor for determining min Neff")
-    parser.add_argument('--target-ptwist', type=float, default=propeller.TARGET_PROPELLER_TWIST,
-                        help="Target persistence length in degrees")
+    parser.add_argument('--target-dist', type=float, default=0.0,
+                        help="Target end to end distance in oxDNA units")
     parser.add_argument('--run-name', type=str,
                         help='Run name')
     parser.add_argument('--max-approx-iters', type=int, default=5,
