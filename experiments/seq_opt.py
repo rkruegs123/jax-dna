8151bc3e779655ca6f69d4b1f14a286adb9466bf
Ryan Krueger <ryankrue@umich.edu>
2024-09-17 14:42:35 -0400

    More seq opt

--- experiments/seq_opt.py
+++ experiments/seq_opt.py
@@ -42,6 +42,8 @@ def run(args):
     gumbel_start = args['gumbel_start']
     gumbel_temps = onp.linspace(gumbel_start, gumbel_end, n_iters)
 
+    use_rg = args['use_rg']
+
     n_sims = args['n_sims']
     n_sample_steps = args['n_sample_steps']
     n_eq_steps = args['n_eq_steps']
@@ -55,7 +57,7 @@ def run(args):
 
     plot_every = args['plot_every']
     run_name = args['run_name']
-    target_dist = args['target_dist']
+    target_obs = args['target_obs']
     max_approx_iters = args['max_approx_iters']
 
     # Setup the logging directoroy
@@ -185,6 +187,27 @@ def run(args):
     def e2e_distance(body):
         return space.distance(displacement_fn(body.center[0], body.center[-1]))
 
+    def rg(body):
+        R = body.center
+        n_states = R.shape[0]
+        mass = jnp.ones(n_states) # assumes equal mass
+
+        com = jnp.sum(jnp.multiply(R, jnp.expand_dims(mass, axis=1)), axis=0) / jnp.sum(mass) # note: assumes free boundary conditions
+
+        drs = vmap(displacement_fn, (None, 0))(com, R)
+        rs = space.distance(drs)
+
+        I = jnp.sum(mass * rs**2)
+        M = jnp.sum(mass)
+        rg = jnp.sqrt(I/M)
+        return rg
+
+    if use_rg:
+        observable_fn = rg
+    else:
+        observable_fn = e2e_distance
+
+
     def get_ref_states(params, init_body, key, i, temp):
 
         curr_logits = params['logits']
@@ -209,27 +232,28 @@ def run(args):
 
         ref_energies = vmap(energy_fn)(ref_states)
 
-        ref_dists = vmap(e2e_distance)(ref_states) # FIXME: this doesn't depend on params...
+        # ref_dists = vmap(e2e_distance)(ref_states) # FIXME: this doesn't depend on params...
+        ref_obs = vmap(observable_fn)(ref_states)
 
-        n_traj_states = len(ref_dists)
-        running_avg_dists = onp.cumsum(ref_dists) / onp.arange(1, n_traj_states + 1)
-        plt.plot(running_avg_dists)
+        n_traj_states = len(ref_obs)
+        running_avg_obs = onp.cumsum(ref_obs) / onp.arange(1, n_traj_states + 1)
+        plt.plot(running_avg_obs)
         plt.savefig(iter_dir / f"running_avg.png")
         plt.close()
 
-        plt.plot(running_avg_dists[-int(n_traj_states // 2):])
+        plt.plot(running_avg_obs[-int(n_traj_states // 2):])
         plt.savefig(iter_dir / f"running_avg_second_half.png")
         plt.close()
 
-        sns.histplot(ref_dists)
-        plt.savefig(iter_dir / "dist_hist.png")
+        sns.histplot(ref_obs)
+        plt.savefig(iter_dir / "obs_hist.png")
         plt.clf()
 
-        return ref_states, ref_energies, ref_dists
+        return ref_states, ref_energies, ref_obs
 
 
     @jit
-    def loss_fn(params, ref_states: rigid_body.RigidBody, ref_energies, ref_dists, temp):
+    def loss_fn(params, ref_states: rigid_body.RigidBody, ref_energies, ref_obs, temp):
         logits = params['logits']
         pseq = normalize(logits, temp)
 
@@ -247,15 +271,15 @@ def run(args):
         weights = boltzs / denom
 
         # Compute the observable
-        expected_dist = jnp.dot(weights, ref_dists)
+        expected_obs = jnp.dot(weights, ref_obs)
 
         # Compute effective sample size
         n_eff = jnp.exp(-jnp.sum(weights * jnp.log(weights)))
 
-        mse = (expected_dist - target_dist)**2
+        mse = (expected_obs - target_obs)**2
         rmse = jnp.sqrt(mse)
 
-        return rmse, (n_eff, expected_dist, pseq)
+        return rmse, (n_eff, expected_obs, pseq)
     grad_fn = value_and_grad(loss_fn, has_aux=True)
     grad_fn = jit(grad_fn)
 
@@ -270,30 +294,31 @@ def run(args):
 
     init_body = conf_info.get_states()[0]
     print(f"Generating initial reference states and energies...")
-    ref_states, ref_energies, ref_dists = get_ref_states(params, init_body, key, 0, temp=gumbel_temps[0])
+    ref_states, ref_energies, ref_obs = get_ref_states(params, init_body, key, 0, temp=gumbel_temps[0])
 
     min_n_eff = int(n_ref_states * min_neff_factor)
     all_losses = list()
-    all_edists = list()
+    all_obs = list()
     all_ref_losses = list()
-    all_ref_edists = list()
+    all_ref_obs = list()
     all_ref_times = list()
 
     loss_path = log_dir / "loss.txt"
     neff_path = log_dir / "neff.txt"
-    dist_path = log_dir / "dist.txt"
+    obs_path = log_dir / "obs.txt"
     argmax_seq_path = log_dir / "argmax_seq.txt"
     argmax_seq_scaled_path = log_dir / "argmax_seq_scaled.txt"
+    grads_path = log_dir / "grads.txt"
 
     num_resample_iters = 0
     for i in tqdm(range(n_iters)):
-        (loss, (n_eff, expected_dist, pseq)), grads = grad_fn(params, ref_states, ref_energies, ref_dists, gumbel_temps[0])
+        (loss, (n_eff, expected_obs, pseq)), grads = grad_fn(params, ref_states, ref_energies, ref_obs, gumbel_temps[0])
 
 
         if i == 0:
             all_ref_losses.append(loss)
             all_ref_times.append(i)
-            all_ref_edists.append(expected_dist)
+            all_ref_obs.append(expected_obs)
 
         if n_eff < min_n_eff or num_resample_iters >= max_approx_iters:
             num_resample_iters
@@ -301,11 +326,11 @@ def run(args):
             print(f"Resampling reference states...")
             key, split = random.split(key)
             # ref_states, ref_energies, ref_dists = get_ref_states(params, ref_states[-1], split, i, temp=gumbel_temps[i])
-            ref_states, ref_energies, ref_dists = get_ref_states(params, init_body, split, i, temp=gumbel_temps[i])
-            (loss, (n_eff, expected_dist, pseq)), grads = grad_fn(params, ref_states, ref_energies, ref_dists, gumbel_temps[i])
+            ref_states, ref_energies, ref_obs = get_ref_states(params, init_body, split, i, temp=gumbel_temps[i])
+            (loss, (n_eff, expected_obs, pseq)), grads = grad_fn(params, ref_states, ref_energies, ref_obs, gumbel_temps[i])
 
             all_ref_losses.append(loss)
-            all_ref_edists.append(expected_dist)
+            all_ref_obs.append(expected_obs)
             all_ref_times.append(i)
 
 
@@ -320,14 +345,19 @@ def run(args):
         with open(argmax_seq_scaled_path, "a") as f:
             f.write(f"{argmax_seq_scaled}\n")
 
+        with open(grads_path, "a") as f:
+            f.write(f"{pprint.pformat(grads)}\n")
+
         with open(loss_path, "a") as f:
             f.write(f"{loss}\n")
-        with open(dist_path, "a") as f:
-            f.write(f"{expected_dist}\n")
+        with open(obs_path, "a") as f:
+            f.write(f"{expected_obs}\n")
         with open(neff_path, "a") as f:
             f.write(f"{n_eff}\n")
         all_losses.append(loss)
-        all_edists.append(expected_dist)
+        all_obs.append(expected_obs)
+
+
 
 
         print(f"Loss: {loss}")
@@ -348,14 +378,14 @@ def run(args):
             plt.clf()
 
             # Plot the persistence lengths
-            plt.plot(onp.arange(i+1), all_edists, linestyle="--", color='blue')
-            plt.scatter(all_ref_times, all_ref_edists, marker='o', label="Resample points", color='blue')
-            plt.axhline(y=target_dist, linestyle='--', label="Target E2E dist", color='red')
+            plt.plot(onp.arange(i+1), all_obs, linestyle="--", color='blue')
+            plt.scatter(all_ref_times, all_ref_obs, marker='o', label="Resample points", color='blue')
+            plt.axhline(y=target_obs, linestyle='--', label="Target obs", color='red')
             plt.xlabel("Iteration")
-            plt.ylabel("Expected E2E Dist (oxDNA units)")
+            plt.ylabel("Expected Observable (oxDNA units)")
             plt.legend()
             # plt.title(f"DiffTRE E2E Dist Optimization, Neff factor={min_neff_factor}")
-            plt.savefig(img_dir / f"edists_iter{i}.png")
+            plt.savefig(img_dir / f"obs_iter{i}.png")
             plt.clf()
 
 def get_parser():
@@ -377,8 +407,8 @@ def get_parser():
                         help="Learning rate")
     parser.add_argument('--min-neff-factor', type=float, default=0.95,
                         help="Factor for determining min Neff")
-    parser.add_argument('--target-dist', type=float, default=0.0,
-                        help="Target end to end distance in oxDNA units")
+    parser.add_argument('--target-obs', type=float, default=0.0,
+                        help="Target end to end distance (or Rg) in oxDNA units")
     parser.add_argument('--run-name', type=str,
                         help='Run name')
     parser.add_argument('--max-approx-iters', type=int, default=5,
@@ -390,6 +420,9 @@ def get_parser():
                         help="Starting temperature for gumbel softmax")
     parser.add_argument('--gumbel-end', type=float, default=0.01,
                         help="End temperature for gumbel softmax")
+
+    parser.add_argument('--use-rg', action='store_true',
+                        help="If true, will use Rg instaed of e2e dist")
 
     return parser
 
