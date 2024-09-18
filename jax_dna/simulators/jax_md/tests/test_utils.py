eb54f5973d15969d6cf39fd3f2e6c99dcc2d407f
Ryan Hausen <ryan.hausen@gmail.com>
2024-09-18 17:40:43 -0400

    formatting and linting on tests

--- jax_dna/simulators/jax_md/tests/test_utils.py
+++ jax_dna/simulators/jax_md/tests/test_utils.py
@@ -1,26 +1,32 @@
 """Tests for the jax_md.utils module."""
 
-import numpy as np
-import pytest
-
 import jax_dna.simulators.jax_md.utils as jdna_utils
+import numpy as np
 
-def test_NoNeighborList_init():
+
+def test_NoNeighborList_init(): # noqa: N802 - class name
     """Test the NoNeighborList __init__ method."""
     nn = jdna_utils.NoNeighborList(
-        unbonded_nbrs=np.array([
-            [1, 2, 3],
-            [4, 5, 6],
-        ])
+        unbonded_nbrs=np.array(
+            [
+                [1, 2, 3],
+                [4, 5, 6],
+            ]
+        )
     )
-    assert np.testing.assert_equal(nn.idx, np.array([
-        [1, 2, 3],
-        [4, 5, 6],
-    ]))
+    assert np.testing.assert_equal(
+        nn.idx,
+        np.array(
+            [
+                [1, 2, 3],
+                [4, 5, 6],
+            ]
+        ),
+    )
 
     assert nn.allocate(None) == nn
     assert nn.update(None) == nn
 
 
-def test_StaticSimulatorParams_init():
+def test_StaticSimulatorParams_init(): # noqa: N802 - class name
     pass
