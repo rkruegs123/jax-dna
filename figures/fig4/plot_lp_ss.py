657218a50f6a4062aed1499f1e0c036f213ed75c
Ryan Krueger <ryankrue@umich.edu>
2024-09-27 16:41:16 -0400

    Adding heatmaps for changs in HB and stack scalars

--- figures/fig4/plot_lp_ss.py
+++ figures/fig4/plot_lp_ss.py
@@ -5,6 +5,7 @@ import pdb
 from matplotlib import rc
 from matplotlib.colors import LinearSegmentedColormap
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 
 from figures.utils import colors, labels
 
@@ -39,6 +40,7 @@ fin_ref_lps = fin_ref_lps[keep_ref_iters]
 
 # for width, height in [(20, 14), (24, 14), (28, 14)]:
 for width, height in [(16, 14)]:
+# for width, height in []:
 
 
     fig, ax = plt.subplots(figsize=(width, height))
@@ -67,3 +69,49 @@ for width, height in [(16, 14)]:
     plt.show()
     # plt.savefig(output_dir / f"lps_ss_{width}x{height}.pdf")
     plt.close()
+
+
+
+def plot_changes(changes, width=12, height=10, title=None):
+    cmap = plt.get_cmap('RdYlGn')  # Red to green continuous colormap
+
+    max_delta = onp.abs(changes).max()
+    vmin = -max_delta
+    vmax = max_delta
+    norm = mcolors.TwoSlopeNorm(vmin=vmin, vcenter=0, vmax=vmax)
+
+
+    fig, ax = plt.subplots(figsize=(12, 10))
+    im = ax.imshow(changes, cmap=cmap, norm=norm, aspect='auto')
+
+
+    labels = ['A', 'C', 'G', 'T']
+
+    # Set the ticks to 'ACGT' on both axes
+    ax.set_xticks(ticks=onp.arange(len(labels)), labels=labels)
+    ax.set_yticks(ticks=onp.arange(len(labels)), labels=labels)
+
+    fig.colorbar(im, orientation='vertical', label="Change")
+    if title is not None:
+        ax.set_title(title)
+
+    plt.tight_layout()
+
+    plt.show()
+    plt.close()
+
+
+# HB weights
+iter0_hb_weights = onp.load(target_dir / "ref_traj" / "iter0" / "hb_weights.npy")
+iter89_hb_weights = onp.load(target_dir / "ref_traj" / "iter89" / "hb_weights.npy")
+delta_hb_weights = iter89_hb_weights - iter0_hb_weights
+
+plot_changes(delta_hb_weights, title="Change in HB Weights")
+
+
+# Stack weights
+iter0_stack_weights = onp.load(target_dir / "ref_traj" / "iter0" / "stack_weights.npy")
+iter89_stack_weights = onp.load(target_dir / "ref_traj" / "iter89" / "stack_weights.npy")
+delta_stack_weights = iter89_stack_weights - iter0_stack_weights
+
+plot_changes(delta_stack_weights, title="Change in Stack Weights")
