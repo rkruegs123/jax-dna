398e7a352a9a63102af2197829e2fe8d1b76111f
Ryan Hausen <ryan.hausen@gmail.com>
2024-10-21 19:41:53 -0400

    wip optimization

--- jax_dna/optimization/base.py
+++ jax_dna/optimization/base.py
@@ -6,11 +6,27 @@ import jax_dna.energy.base as jdna_energy
 import jax_dna.energy.configuration as jdna_energy_config
 import jax_dna.losses.observable_wrappers as jdna_losses
 import jax_dna.simulators.base as jdna_simulators
+import jax_dna.simulators.io as jdna_sio
 import jax_dna.utils.types as jdna_types
+import ray
 
+
+META_DATA = dict[str, Any]
 
 class Objective:
     loss_fn: jdna_losses.LossFn
+
+
+@ray.remote
+class SimulatorActor:
+    def __init__(self, simulator: jdna_simulators.BaseSimulation, meta_data: META_DATA):
+        self.simulator = simulator
+        self.meta_data = meta_data
+
+    def run(self, params: jdna_types.Params) -> tuple[jdna_sio.SimulatorTrajectory, jdna_sio.SimulatorMetaData, META_DATA]:
+        sim_traj, sim_meta =  self.simulator.run(params, self.meta_data)
+        return sim_traj, sim_meta, self.meta_data
+
 
 
 @chex.dataclass(frozen=True)
@@ -19,7 +35,7 @@ class Optimization:
     energy_configs: list[jdna_energy_config.BaseConfiguration]
     objectives: list[Objective]
     aggregate_grad_fn: typing.Callable[[list[jdna_types.Grads]], jdna_types.Grads]
-    simulators: list[jdna_simulators.BaseSimulation]
+    simulators: list[tuple[SimulatorActor, META_DATA]]
 
     def __post_init__(self):
         # check that the energy functions and configurations are compatible
@@ -27,12 +43,20 @@ class Optimization:
         pass
 
     def step(self) -> tuple["Optimization", list[jdna_types.Grads]]:
+        # run the simulators
+        sim_results = [sim.run.remote() for sim, _ in self.simulators]
+
+        # wait for the simulators to finish
+
+        # collect the results and match them up to the objectives
         pass
 
     def update_params(params: jdna_types.Params, grads: jdna_types.Grads) -> jdna_types.Params:
+        # aggreagete function
         pass
 
     def post_step() -> Any:
+
         pass
 
 
--- jax_dna/simulators/io.py
+++ jax_dna/simulators/io.py
@@ -1,20 +1,49 @@
 """Common data structures for simulator I/O."""
 
+from typing import Any
 import chex
 import jax.numpy as jnp
 import jax_md
 
 
 @chex.dataclass()
-class SimulatorTrajectory:
-    """A trajectory of a simulation run."""
+class SimulatorMetaData:
+    """Metadata for a simulation run."""
 
     seq_oh: jnp.ndarray
     strand_lengths: list[int]
+    grads: jax_md.rigid_body.RigidBody|None = None
+    misc_data: dict[str, Any] = {}
+
+    def slice(self, key: int | slice) -> "SimulatorMetaData":
+        """Slice the metadata."""
+
+        new_grads = self.grads
+        if new_grads:
+            centers = [{k:c[k][key] for k in c} for c in self.grads.center]
+            orientations = [
+                {k:jax_md.rigid_body.Quaternion(vec=c[k].vec[key]) for k in c}
+                for c in self.grads.orientation
+            ]
+
+            new_grads = jax_md.rigid_body.RigidBody(
+                center = centers,
+                orientation = orientations,
+            )
+
+        return self.replace(
+            grads=new_grads,
+        )
+
+
+@chex.dataclass()
+class SimulatorTrajectory:
+    """A trajectory of a simulation run."""
     rigid_body: jax_md.rigid_body.RigidBody
 
     def slice(self, key: int | slice) -> "SimulatorTrajectory":
         """Slice the trajectory."""
+
         return self.replace(
             rigid_body=self.rigid_body[key],
         )
