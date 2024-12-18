4e263416bd1b17816e83170e14a6afe13e385a06
Ryan Krueger <ryankrue@umich.edu>
2024-12-17 21:49:06 -0500

    Helper functions for WLC fit

--- jax_dna/utils/units.py
+++ jax_dna/utils/units.py
@@ -1,3 +1,10 @@
 """Units for the oxDNA model."""
 
 ANGSTROMS_PER_OXDNA_LENGTH = 8.518
+ANGSTROMS_PER_NM = 10
+NM_PER_OXDNA_LENGTH = ANGSTROMS_PER_OXDNA_LENGTH / ANGSTROMS_PER_NM
+PN_PER_OXDNA_FORCE = 48.63
+
+def get_kt(t_kelvin):
+    """Converts a temperature in Kelvin to kT in simulation units."""
+    return 0.1 * t_kelvin / 300.0
